fb409a504649e11c8bb6a8306cafb9292b37ad10
Jonattan <jonattanva89@gmail.com>
2022-02-01 16:06:27 -0500

    Se soluciona error al incluir una separación entre elementos

--- src/component/virtual/index.item.tsx
+++ src/component/virtual/index.item.tsx
@@ -1,20 +1,21 @@
 type PropTypes = {
-  children: React.ReactNode;
-  height: number;
-  padding: number;
-  width: number;
+    children: React.ReactNode;
+    height: number;
+    padding: number;
+    width: number;
 };
 
 const Item = (props: PropTypes) => (
-  <div
-    style={{
-      height: `${props.height}px`,
-      padding: `${props.padding}px`,
-      width: `${props.width}px`,
-    }}
-  >
-    {props.children}
-  </div>
+    <div
+        className="monolieta-virtual-scroll-item"
+        style={{
+            height: `${props.height}px`,
+            padding: `${props.padding}px`,
+            width: `${props.width}px`,
+        }}
+    >
+        {props.children}
+    </div>
 );
 
 export default Item;
--- src/component/virtual/index.row.tsx
+++ src/component/virtual/index.row.tsx
@@ -1,9 +1,9 @@
 type PropTypes = {
-  children: React.ReactNode;
+    children: React.ReactNode;
 };
 
 const Row = (props: PropTypes) => (
-  <div className="monolieta-virtual-scroll__row">{props.children}</div>
+    <div className="monolieta-virtual-scroll__row">{props.children}</div>
 );
 
 export default Row;
